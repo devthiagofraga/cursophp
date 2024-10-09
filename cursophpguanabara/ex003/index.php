70a58adebea94b501a0379b79c5c739942a5a5e7
devthiagofraga <thimefra@gmail.com>
2024-10-09 17:19:37 -0300

    aula formularios

---
+++ cursophpguanabara/ex003/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        // $num = 300;
+        // echo "O valor da variável é $num";
+
+        // $v = 3.25;
+        // var_dump($v)
+        // $vet = [6, 2, 9, 3, 5];
+        // var_dump($vet)
+
+        const CANAL = " Curso em vídeo";
+        echo "Eu assisto o canal". CANAL
+    ?>
+</body>
+</html>
